5635d0ba0f288ea99154a8075b587b71348af082
Daniel Roe <daniel@roe.dev>
2020-09-06 09:21:01 +0200

    test: update lint rule for new version

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
   plugins: ['jest'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-inferrable-types': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    'no-unused-vars': 'off',
   },
   extends: ['@siroc'],
 }
